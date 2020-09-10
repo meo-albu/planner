4a5aac4034c83ed8ba8793d2c05acd302a635d03
Marian Albu <albumeo1990@gmail.com>
2020-09-11 01:48:52 +0300

    error handling, ui changes

--- src/index.js
+++ src/index.js
@@ -5,19 +5,15 @@ import App from './App';
 import {Provider} from 'react-redux'
 import rootReducer from './Store/reducers'
 // eslint-disable-next-line
-import {createStore, applyMiddleware, compose} from 'redux'
-import thunk from 'redux-thunk'
+import {createStore} from 'redux'
 
 const screen = window.screen.availWidth
 let store = ''
 
 if(screen > 700) {
-  store = createStore(rootReducer, compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  ))
+  store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 } else {
-  store = createStore(rootReducer, applyMiddleware(thunk))
+  store = createStore(rootReducer)
 }
 
 ReactDOM.render(
